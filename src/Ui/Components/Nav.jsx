b1bdc5f623b39e171d9ad9f4f36d037a6d67adb7
JulianyEufrasio <julianyeufrasio23@gmail.com>
2024-07-08 10:25:40 -0300

    Criando Nav.jsx + App.css base

---
+++ src/Ui/Components/Nav.jsx
@@ -0,0 +1,19 @@
+import '../Styles/Nav.css';
+
+function Nav(){
+    return(
+        <nav className='navbar'>
+            <span className="logo">Miaumigo</span>
+            <ul className="navlinks">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Adote um amigo</a></li>
+                <li><a href="#">Divulgue um animal</a></li>
+                <li><a href="#">Ajude-nos</a></li>
+                <li><a href="#">Sobre nós</a></li>
+            </ul>
+            <button type="submit" className='btn'>Entrar</button>
+        </nav>
+    );
+}
+
+export default Nav;
